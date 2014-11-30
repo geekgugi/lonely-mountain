9b48a1f0bcc9dc11022b609af53e9d46579ab313
Sagar Gugwad <saagar.gugwad@gmail.com>
2014-11-30 17:02:45 +0530

    Added angular-bootstrap for tabs.
    Settings page will have dynamic content. Lots dummy content added
    can be extended later.

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,21 +1,33 @@
 'use strict';
 
 angular.module('lmApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
-    $scope.errors = {};
+.controller('SettingsCtrl', function ($scope, User, Auth, $window) {
+  $scope.errors = {};
+  $scope.tabs = [
+    { title:'Profile1', content:'Lets generate dynamic content' },
+    { title:'Profile2', content:'Disabled content', disabled: true }
+  ];
 
-    $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
-      }
-		};
-  });
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      $window.alert('You\'ve selected the alert tab!');
+    });
+  };
+
+  $scope.changePassword = function(form) {
+    $scope.submitted = true;
+    if(form.$valid) {
+      // Change this code suitably
+      // Auth.updateProfile();
+      Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
+      .then( function() {
+        $scope.message = 'Profile updated successfully.';
+      })
+      .catch( function() {
+        form.password.$setValidity('mongoose', false);
+        $scope.errors.other = 'Incorrect password';
+        $scope.message = '';
+      });
+    }
+  };
+});
