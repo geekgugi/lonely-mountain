0438036cacb3dbe321e3d8299c88dbca04ba64c1
Sagar Gugwad <saagar.gugwad@gmail.com>
2014-12-01 19:47:34 +0530

    Marking example added

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,7 +2,16 @@
 
 angular.module('lmApp')
 .controller('MainCtrl', function ($scope, $http, socket, uiGmapGoogleMapApi) {
-  $scope.map = {};
+  $scope.map = {
+    center: {
+      latitude: 12.95,
+      longitude: 77.777
+    },
+    zoom: 14
+  };
+  $scope.options = {
+    scrollwheel: false
+  };
 
   $scope.loadCurrentLocation = function() {
     uiGmapGoogleMapApi.then(function() {
@@ -21,14 +30,19 @@ angular.module('lmApp')
       zoom: 14
     };
   };
-
+  $scope.dummyMarks = [];  
+  var mark1 = {
+    latitude: 12.95,
+    longitude: 77.77,
+    title: 'm' + 1
+  }
+  mark1['id'] = 1;
+  $scope.dummyMarks.push(mark1);
   $scope.search = function() {
-    
     $scope.map.coords = {};
     $scope.map.coords.latitude = $scope.details.geometry.location.lat();
     $scope.map.coords.longitude = $scope.details.geometry.location.lng();
     $scope.map.zoom = 14;
-    changeMap($scope.map);
   };
-  $scope.loadCurrentLocation();
+//  $scope.loadCurrentLocation();
 });
