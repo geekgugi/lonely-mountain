c0595c98c80abb88b3ad0ecfb6035a32e3eeb06f
Sagar Gugwad <saagar.gugwad@gmail.com>
2014-12-03 20:16:29 +0530

    Removing search button.
    Because of search scroll and draggable was not working.
    We need to stick to angular directives.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('lmApp')
-.controller('MainCtrl', function ($scope, $http, socket, uiGmapGoogleMapApi) {
+.controller('MainCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
+  // map variable will help to load map
+  $scope.marks = []; 
   $scope.map = {
+    control: {},
     center: {
       latitude: 12.95,
       longitude: 77.777
@@ -14,7 +17,7 @@ angular.module('lmApp')
   };
 
   $scope.loadCurrentLocation = function() {
-    uiGmapGoogleMapApi.then(function() {
+    uiGmapGoogleMapApi.then(function(map) {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(changeMap);
       }
@@ -30,6 +33,7 @@ angular.module('lmApp')
       zoom: 14
     };
   };
+  // Can be removed
   $scope.dummyMarks = [];  
   var mark1 = {
     latitude: 12.95,
@@ -38,11 +42,25 @@ angular.module('lmApp')
   }
   mark1['id'] = 1;
   $scope.dummyMarks.push(mark1);
+  // removed from here
+  
   $scope.search = function() {
     $scope.map.coords = {};
     $scope.map.coords.latitude = $scope.details.geometry.location.lat();
     $scope.map.coords.longitude = $scope.details.geometry.location.lng();
     $scope.map.zoom = 14;
+  };
+
+  $scope.findPlaces = function () {
+    var selectedLatitude =  $scope.details.geometry.location.lat();
+    var selectedLongitude = $scope.details.geometry.location.lng();
+    //  TODO: Maximum distance has to be computed from the UI
+    //  i) By making use of boundries
+    //  ii) By making use of circle
+    var maximumDistance = "12"
+    // make request with the these selected values
+    // to get all the places available within fixed range
+
   };
 //  $scope.loadCurrentLocation();
 });
